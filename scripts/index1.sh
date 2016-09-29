8d6d32b9de9dbda2a62dacc9399862c25db66187
Vesa Meskanen <vesa.meskanen@logica.com>
2016-09-29 07:37:03 +0300

    Rebalance import tasks to match fixed nlsfi-places import load
    Also, use 2 parallel openaddresses import processes

--- scripts/index1.sh
+++ scripts/index1.sh
@@ -5,21 +5,9 @@
 # errors should break the execution
 set -e
 
-# param: zip name containing gtfs data
-function import_gtfs {
-    unzip -o $1
-    prefix=$(echo $1 | sed 's/.zip//g')
-    prefix=${prefix^^}
-    node $TOOLS/pelias-gtfs/import -d $DATA/router-finland --prefix=$prefix
-}
+node $TOOLS/pelias-nlsfi-places-importer/lib/index -d $DATA/nls-places
 
-cd $DATA/router-finland
-targets=(`ls *.zip`)
-for target in "${targets[@]}"
-do
-    import_gtfs $target
-done
-
-node --max-old-space-size=6200 $TOOLS/pelias-nlsfi-places-importer/lib/index -d $DATA/nls-places
+node $TOOLS/polylines/bin/cli.js --config --db
+node $TOOLS/openstreetmap/index
 
 echo 'OK' >> /tmp/indexresults
