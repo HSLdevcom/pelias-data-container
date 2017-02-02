f98fb6d741cc062c9f21a9d0b00c1bc940e6bcec
Vesa Meskanen <vesa.meskanen@cgi.com>
2017-02-02 13:55:50 +0200

    Update NLSFI data entry url

--- scripts/nlsfi-loader.sh
+++ scripts/nlsfi-loader.sh
@@ -7,7 +7,7 @@ mkdir -p $DATA/nls-places
 cd $DATA/nls-places
 
 # Download nls paikat data
-curl -sS -O http://kartat.kapsi.fi/files/nimisto/paikat/etrs89/gml/paikat_2016_01.zip
+curl -sS -O http://kartat.kapsi.fi/files/nimisto/paikat/etrs89/gml/paikat_2017_01.zip
 unzip paikat_2016_01.zip
 rm paikat_2016_01.zip
 
