e3856d91b4d8ac71a009b14a7c94c631df3bc5d8
Vesa Meskanen <vesa.meskanen@cgi.com>
2017-02-03 09:35:13 +0200

    Fix data file name references, and use a variable to avoid bugs in name changes

--- scripts/nlsfi-loader.sh
+++ scripts/nlsfi-loader.sh
@@ -3,12 +3,15 @@
 # errors should break the execution
 set -e
 
+NAME=paikat_2017_01.zip
+
 mkdir -p $DATA/nls-places
 cd $DATA/nls-places
 
 # Download nls paikat data
-curl -sS -O http://kartat.kapsi.fi/files/nimisto/paikat/etrs89/gml/paikat_2017_01.zip
-unzip paikat_2016_01.zip
-rm paikat_2016_01.zip
+curl -sS -O http://kartat.kapsi.fi/files/nimisto/paikat/etrs89/gml/$NAME
+unzip $NAME
+rm $NAME
 
+echo '##### Loaded nlsfi data'
 echo 'OK' >> /tmp/loadresults
