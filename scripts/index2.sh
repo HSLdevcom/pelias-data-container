82b849d6be1cff5d4ea14fc2ac816b5c82b3062c
Vesa Meskanen <vesa.meskanen@cgi.com>
2017-05-16 11:54:24 +0300

    Import gtfs from waltti and finland, prefix source by feedid, not by gtfs file name.

--- scripts/index2.sh
+++ scripts/index2.sh
@@ -6,17 +6,26 @@ set -e
 # param: zip name containing gtfs data
 function import_gtfs {
     unzip -o $1
-    prefix=$(echo $1 | sed 's/.zip//g')
+
+    # extract feed id
+    index=$(sed -n $'1s/,/\\\n/gp' feed_info.txt | grep -nx 'feed_id' | cut -d: -f1)
+    prefix=$(cat feed_info.txt | sed -n 2p | cut -d "," -f $index)
     prefix=${prefix^^}
     node $TOOLS/pelias-gtfs/import -d $DATA/router-finland --prefix=$prefix
 }
 
-cd $DATA/router-finland
-targets=(`ls *.zip`)
-for target in "${targets[@]}"
-do
-    import_gtfs $target
-done
+function import_router {
+    cd $DATA/$1
+    targets=(`ls *.zip`)
+    for target in "${targets[@]}"
+    do
+        import_gtfs $target
+    done
+}
+
+import_router router-finland
+import_router router-waltti
+
 echo '###### gtfs done'
 
 #import openaddresses data
