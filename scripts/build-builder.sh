c994e039094063e1ff01be61dae450569979efc1
Vesa Meskanen <vesa.meskanen@cgi.com>
2017-06-20 14:05:34 +0300

    Add travis build
    Travis will build the base image and the builder image. Builder image
    will be run in azure and it will create the actual data container.

---
+++ scripts/build-builder.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Set these environment variables
+#DOCKER_USER // dockerhub credentials
+#DOCKER_AUTH
+#DOCKER_TAG // build id
+#ORG // optional
+
+set -e
+
+ORG=${ORG:-hsldevcom}
+DOCKER_IMAGE=pelias-data-container-builder
+DOCKER_TAG=${DOCKER_TAG:-$TRAVIS_BUILD_ID}
+DOCKER_TAGGED_IMAGE=$ORG/$DOCKER_IMAGE:$DOCKER_TAG
+
+# Build image
+docker build -t="$DOCKER_TAGGED_IMAGE" -f Dockerfile.builder
+
+docker login -u $DOCKER_USER -p $DOCKER_AUTH
+docker push $ORG/$DOCKER_IMAGE:$DOCKER_TAG
+docker tag -f $ORG/$DOCKER_IMAGE:$DOCKER_TAG $ORG/$DOCKER_IMAGE:latest
+docker push $ORG/$DOCKER_IMAGE:latest
+
+echo "$DOCKER_IMAGE built and deployed"
