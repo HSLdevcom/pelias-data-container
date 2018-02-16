c55a88751d8f659c11bf28e545ee095d9b2a3732
Vesa Meskanen <vesa.meskanen@cgi.com>
2018-02-16 14:51:18 +0200

    Revert to older mml data package if newest is broken

--- scripts/nlsfi-loader.sh
+++ scripts/nlsfi-loader.sh
@@ -8,14 +8,28 @@ cd $DATA/nls-places
 
 URL='http://kartat.kapsi.fi/files/nimisto/paikat/etrs89/gml/'
 
-# find out latest zip filename
-NAME=$(curl -Ss $URL | grep -o 'href=".*zip"' | sort -r | head -1 | sed 's/\(href=\|"\)//g')
+function get_name {
+    # find out latest zip filename
+    NAME=$(curl -Ss $URL | grep -o 'href=".*zip"' | sort -r | head $1 | tail -1 | sed 's/\(href=\|"\)//g')
+}
 
-echo 'Loading nlsfi data from' $URL$NAME
+get_name -1
 
 # Download nls paikat data
+set +e
+echo 'Loading nlsfi data from' $URL$NAME
 curl -sS -O --fail $URL$NAME
 unzip -o $NAME
+RESULT=$?
 rm $NAME
+
+if [ $RESULT -ne 0 ]; then
+    get_name -2
+    echo 'Bad data package, trying older version' $URL$NAME
+    curl -sS -O --fail $URL$NAME
+    set -e
+    unzip -o $NAME
+    rm $NAME
+fi
 
 echo '##### Loaded nlsfi data'
