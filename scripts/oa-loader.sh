3dbec6c9fa9c2a29897b04b5ce5ab154ceac6763
Vesa Meskanen <vesa.meskanen@cgi.com>
2018-11-08 14:51:16 +0200

    Prepare for countrywise OA data
    Can't optimize sv loading by smaller areas any more

--- scripts/oa-loader.sh
+++ scripts/oa-loader.sh
@@ -12,18 +12,4 @@ curl -sS --fail http://results.openaddresses.io/state.txt | sed 's/\s\+/\n/g' | 
 ls *.zip | xargs -n 1 unzip -o
 rm *.zip README.*
 
-#do some cleanup for redundant entries
-rm -f fi/ahvenanmaa-fi.csv
-rm -f fi/etelä-karjala-sv.csv
-rm -f fi/etelä-savo-sv.csv
-rm -f fi/kainuu-sv.csv
-rm -f fi/kanta-häme-sv.csv
-rm -f fi/keski-suomi-sv.csv
-rm -f fi/lappi-sv.csv
-rm -f fi/päijät-häme-sv.csv
-rm -f fi/pirkanmaa-sv.csv
-rm -f fi/pohjois-karjala-sv.csv
-rm -f fi/pohjois-pohjanmaa-sv.csv
-rm -f fi/pohjois-savo-sv.csv
-
 echo '##### Loaded OpenAddresses data'
