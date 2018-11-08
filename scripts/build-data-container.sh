48d9ee43ab9c143ee14cb326ce946d880b8f3340
Vesa Meskanen <vesa.meskanen@cgi.com>
2018-11-08 10:36:25 +0200

    Pass env var through docker layers

--- scripts/build-data-container.sh
+++ scripts/build-data-container.sh
@@ -61,7 +61,7 @@ function build {
 
     DOCKER_TAGGED_IMAGE=$1
     echo "Building $DOCKER_TAGGED_IMAGE"
-    docker build --no-cache -t="$DOCKER_TAGGED_IMAGE" -f Dockerfile.loader .
+    docker build --no-cache --build-arg MMLAPIKEY -t="$DOCKER_TAGGED_IMAGE" -f Dockerfile.loader .
     echo 0 >/tmp/build_ok
 }
 
