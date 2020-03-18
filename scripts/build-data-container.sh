8838105e539ed6676e98688b5e596243d351d888
Vesa Meskanen <vesa.meskanen@cgi.com>
2020-03-18 20:04:53 +0200

    Back to normal threshold

--- scripts/build-data-container.sh
+++ scripts/build-data-container.sh
@@ -25,7 +25,7 @@ DATA_CONTAINER_IMAGE=$ORG/$DOCKER_IMAGE:$DOCKER_TAG
 BASE_IMAGE=$ORG/pelias-data-container-base:$DOCKER_TAG
 
 #Threshold value for regression testing, as %
-THRESHOLD=${THRESHOLD:-20}
+THRESHOLD=${THRESHOLD:-2}
 #how often data is built (default once a day)
 BUILD_INTERVAL=${BUILD_INTERVAL:-1}
 #Substract one day, because first wait hours are computed before each build
