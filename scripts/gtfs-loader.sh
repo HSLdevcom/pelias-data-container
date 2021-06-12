332b33a0821cf3ec960ba5dfbe55e1d7c36e3056
Vesa Meskanen <vesa.meskanen@cgi.com>
2021-06-12 16:26:40 +0300

    chore: extract only gtfs zips

--- scripts/gtfs-loader.sh
+++ scripts/gtfs-loader.sh
@@ -14,19 +14,19 @@ URL="http://api.digitransit.fi/routing-data/v2/"
 SERVICE="finland/"
 NAME="router-finland.zip"
 curl -sS -O --fail $URL$SERVICE$NAME
-unzip -o $NAME && rm $NAME
+unzip -o $NAME *.zip && rm $NAME
 mv router-finland/*.zip gtfs/
 
 SERVICE="waltti/"
 NAME="router-waltti.zip"
 curl -sS -O --fail $URL$SERVICE$NAME
-unzip -o $NAME && rm $NAME
+unzip -o $NAME *.zip && rm $NAME
 mv router-waltti/*.zip gtfs/
 
 SERVICE="hsl/"
 NAME="router-hsl.zip"
 curl -sS -O --fail $URL$SERVICE$NAME
-unzip -o $NAME && rm $NAME
+unzip -o $NAME *.zip && rm $NAME
 mv router-hsl/*.zip gtfs/
 
 echo '##### Loaded GTFS data'
