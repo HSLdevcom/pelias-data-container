0bf1c3aecd5e76b2d99dff6a186924ae5488e8f8
Vesa Meskanen <vesa.meskanen@cgi.com>
2023-08-30 16:37:57 +0300

    Restore Joel's fix

--- scripts/install-tools.sh
+++ scripts/install-tools.sh
@@ -28,7 +28,7 @@ set -x
 set -e
 apt-get update
 echo 'APT::Acquire::Retries "20";' >> /etc/apt/apt.conf
-apt-get install -y --no-install-recommends git unzip python python-pip python-dev build-essential gdal-bin rlwrap procps emacs curl
+apt-get install -y --no-install-recommends git unzip python3 python-pip python-dev build-essential gdal-bin rlwrap procps emacs curl
 rm -rf /var/lib/apt/lists/*
 
 mkdir -p $SCRIPTS
