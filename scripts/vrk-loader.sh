d939a9c3cd42fd75a8459d09ebbb6d7698a15d37
Vesa Meskanen <vesa.meskanen@cgi.com>
2023-08-30 16:30:38 +0300

    No need to convert encoding any more

--- scripts/vrk-loader.sh
+++ scripts/vrk-loader.sh
@@ -10,8 +10,12 @@ echo 'Loading DVV data...'
 curl -sS -O -L --fail  https://geocoding.blob.core.windows.net/vrk/fi_vrk_addresses.zip
 
 unzip -o fi_vrk_addresses.zip
-#convert to utf-8
-ls *.OPT | xargs -n 1 iconv -f ISO-8859-1 -t UTF-8 -o vrk.txt
-rm *.zip *.OPT
+
+mv *.OPT vrk.txt
+
+#convert to utf-8 - no longer needed because csv-parser now accepts encoding parameter
+#ls *.OPT | xargs -n 1 iconv -f ISO-8859-1 -t UTF-8 -o vrk.txt
+
+rm *.zip
 
 echo '##### Loaded DVV data'
