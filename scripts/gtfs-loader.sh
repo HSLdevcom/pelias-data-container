9a63f8b283f562539db648e61df9245fbb90c754
Vesa Meskanen <vesa.meskanen@cgi.com>
2024-09-13 14:53:40 +0300

    Clean up more

--- scripts/gtfs-loader.sh
+++ scripts/gtfs-loader.sh
@@ -11,16 +11,12 @@ cd $DATA
 mkdir -p gtfs
 mkdir -p openstreetmap
 
-DATA_API="https://api.digitransit.fi/routing-data/"
-DEV_DATA_API="https://dev-api.digitransit.fi/routing-data/"
 PARAMS='?'"$API_SUBSCRIPTION_QUERY_PARAMETER_NAME"'='"$API_SUBSCRIPTION_TOKEN"
 
 if [ "$BUILDER_TYPE" = "dev" ]; then
-    URL=$DEV_DATA_API
-    WALTTI_ALT_URL="$DEV_DATA_API"v3/waltti-alt/
+    URL="https://dev-api.digitransit.fi/routing-data/"
 else
-    URL=$DATA_API
-    WALTTI_ALT_URL="$DATA_API"v3/waltti-alt/
+    URL="https://api.digitransit.fi/routing-data/"
 fi
 
 # param1: data version, v2 or v3
